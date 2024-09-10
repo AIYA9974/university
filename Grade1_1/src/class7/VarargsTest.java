11d27c1c97a4b57a5f129cd428510967107ad704
AIYA9974 <159003189+AIYA9974@users.noreply.github.com>
2024-09-10 17:03:38 +0900

    class 7
    END

---
+++ Grade1_1/src/class7/VarargsTest.java
@@ -0,0 +1,19 @@
+package class7;
+
+public class VarargsTest {
+	public static void doSomethings(String... strs) {
+		System.out.print("Arguments are: ");
+		for (String s : strs) {
+			System.out.print(s + ", ");
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		doSomethings("Hello","world","again","and","again");
+		doSomethings("Hello","world");
+		
+		String[] strs = {"apple","orange"};
+		doSomethings(strs);
+	}
+}
